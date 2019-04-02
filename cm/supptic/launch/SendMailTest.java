924ed17c00c1334d7c765b1b22cbf3c7caf63a8c
Noumodong <oreolnoumodong@gmail.com>
2019-04-02 12:37:06 +0200

    gestion envoir par mail

--- cm/supptic/launch/SendMailTest.java
+++ cm/supptic/launch/SendMailTest.java
@@ -24,12 +24,11 @@ public class SendMailTest {
         final String username = "oreolnoumodong@gmail.com";
         final String password = "bihoncare2018";
 
-        Properties props = new Properties();
+        Properties props = System.getProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-//        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.port", 587);
 //        props.put("mail.smtp.host", "smtp.gmail.com");
 //        props.put("mail.smtp.port", "587");
 
